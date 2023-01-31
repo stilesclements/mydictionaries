73c379eba3cdba94fa1414fec1a573bab17cc3b0
Johnny Bhojwani <johnnyvb@gmail.com>
2023-01-31 12:59:53 -0600

    added exercises to person dictionary

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,21 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+#print(person)
+
+# print out the name of the second child
+
+
+
+# print out the name of the cat
+
+
+
+# use a loop to print out the names of each child
+
+
+
+
+# use a loop to print out the pets in the following format:
+# The type of pet is: dog and the name of the pet is: Fido
