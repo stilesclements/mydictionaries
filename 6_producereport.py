39c34219e92518281bad1632fa0f40894145e7cd
Johnny Bhojwani <johnnyvb@gmail.com>
2023-02-01 12:10:33 -0600

    updated produce stated total for Cel & Brussel

--- 6_producereport.py
+++ 6_producereport.py
@@ -48,7 +48,7 @@ ProduceDictionary={
     'Celery': {
         'cost': 3.07,
         'amt_sold': 18.5,
-        'total': 56.8
+        'total': 56.79
     },
     'Spinach': {
         'cost': 4.12,
@@ -193,7 +193,7 @@ ProduceDictionary={
     'Brussels sprouts': {
         'cost': 1.65,
         'amt_sold': 22.9,
-        'total': 37.79
+        'total': 37.78
     },
     'Kale': {
         'cost': 5.02,
